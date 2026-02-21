6a9fcbf62b47ac4780e613d8d2c8a1da8d0bc2b0
Stacey Sitton <stacey.sitton@student.swic.edu>
2026-02-21 12:31:24 -0600

    Add Personal Information variables

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello via Bun (plain JS)!");
+//Personal Information
+const firstName = "Stacey";
+const lastName = "Sitton";
+const age = 43;
+const isStudent = true;
+
+//Using template literals
+const introduction = `Hello! My name is ${firstName} ${lastName}. I am
+ ${age} years old.`;
+const studentStatus = `Am I a student? ${isStudent}`;
+
+//Print in console
+console.log(introduction);
+console.log(studentStatus);
